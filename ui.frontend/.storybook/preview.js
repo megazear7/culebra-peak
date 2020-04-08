d10a862e0e7b4fd7ffe0f35cf3bdf6c84c98beda
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2020-04-08 15:07:49 -0400

    Configure Storybook AEM

---
+++ ui.frontend/.storybook/preview.js
@@ -0,0 +1,8 @@
+import { addParameters, addDecorator } from '@storybook/client-api';
+import { aemMetadata } from '@storybook/aem';
+addDecorator(aemMetadata({
+  components: [
+  ],
+  models: {
+  }
+}));
